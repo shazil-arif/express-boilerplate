c4882d620d262be72a8c6da4c55f0dad5b25535c
Shazil Arif <shazilarif94@gmail.com>
2020-05-08 12:52:24 -0400

    Update blog.js

--- blog.js
+++ blog.js
@@ -8,5 +8,6 @@ const blogSchema = new mongoose.Schema({
     }
 })
 
-const Blog = mongoose.model("blog",blogSchema);
+const Blog = mongoose.model("blog",blogSchema); //note it should not be 'new mongoose.mode()' because that would export a single instance
+//of the model
 module.exports = Blog;
